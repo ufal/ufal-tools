d9694aec15becd598aa7f41069bb52f5a8d1de28
Ondrej Platek <ondrej.platek@seznam.cz>
2024-07-18 15:16:03 +0000

    save notes

--- websocket-demo/server.py
+++ websocket-demo/server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 import logging
 import simple_websocket
+
 app = Flask(__name__)
 
 
-@app.route('/echo', websocket=True)
+@app.route("/wsapp1/echo", websocket=True)
 def echo():
     ws = simple_websocket.Server(request.environ)
     try:
@@ -14,4 +15,4 @@ def echo():
             ws.send(f"data from server {data}")
     except simple_websocket.ConnectionClosed:
         logging.warning(f"connection closed")
-    return ''
+    return ""
